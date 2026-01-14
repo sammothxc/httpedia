d9fe2228c33b975b5f50ffd641ee69709fedc017
Sam Warr <sam.warr.tx@gmail.com>
2026-01-14 09:51:47 -0700

    fixed parsing on certain links

--- httpedia.py
+++ httpedia.py
@@ -233,4 +233,4 @@ def clean_text(text):
     return text.strip()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
