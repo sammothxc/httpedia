d8a09aea08b76b556d35550b5140d262bcb2e1a6
Sam Warr <sam.warr.tx@gmail.com>
2026-01-18 00:15:42 -0700

    html validation check

--- httpedia.py
+++ httpedia.py
@@ -30,7 +30,7 @@ HEADERS = {
 }
 
 
-DOCTYPE = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 2.0//EN">'
+DOCTYPE = '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">'
 
 
 META = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'
